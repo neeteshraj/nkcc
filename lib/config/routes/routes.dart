771d78d0f618520b291067075440f05841979abb
Faseeh Hyder <faseehhyder@duck.com>
2024-12-14 20:34:08 +0500

    fixed

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:support/features/home/presentation/home_screen.dart';
 import 'package:support/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:support/features/not_found_screen/presentation/not_found_screen.dart';
-import 'package:support/features/qr_scan/presentation/screens/qr_screen.dart';
 
 class Routes {
   static const String home = '/home';
@@ -15,8 +14,8 @@ class Routes {
         return MaterialPageRoute(builder: (context) => const HomeScreen());
       case onboarding:
         return MaterialPageRoute(builder: (context) => const OnboardingScreen());
-      case qrcode:
-        return MaterialPageRoute(builder: (context)=> const QRCodeScreen());
+      // case qrcode:
+      //   return MaterialPageRoute(builder: (context)=> const QRCodeScreen());
       default:
         return MaterialPageRoute(builder: (context) => const NotFoundScreen());
     }
