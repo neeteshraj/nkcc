908be34577d243c351a172d6021ba35a0c332cf8
Nitesh Raj Khanal <nitesh.khanal19@gmail.com>
2024-12-23 10:15:16 +0545

    fix: commented firebase messaging service

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,8 @@ void main() async {
     return true;
   };
 
-  FirebaseMessagingService firebaseMessagingService = FirebaseMessagingService();
-  await firebaseMessagingService.initialize();
+  // FirebaseMessagingService firebaseMessagingService = FirebaseMessagingService();
+  // await firebaseMessagingService.initialize();
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
